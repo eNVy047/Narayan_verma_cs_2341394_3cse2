8ef414535ac0f13246f637ed88f0168a3bb1f3c3
Narayan Verma <148738788+eNVy047@users.noreply.github.com>
2026-04-14 18:44:33 +0530

    Time: 0 ms (100%), Space: 42.9 MB (55.9%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -0,0 +1,17 @@
+// class Solution {
+//     public String toLowerCase(String s) {
+//         return s.toLowerCase();
+//     }
+// }
+
+class Solution {
+    public String toLowerCase(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] = (char) (chars[i] + 32);
+            }
+        }
+        return new String(chars);
+    }
+}
