53a1efbbe995fc6a613808b8c789a59c2e191cba
Narayan Verma <148738788+eNVy047@users.noreply.github.com>
2026-04-22 10:07:52 +0530

    Time: 0 ms (100%), Space: 43.9 MB (55.41%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -1,14 +1,37 @@
+// class Solution {
+//     private TreeNode prev = null;
+
+//     public void flatten(TreeNode root) {
+//         if (root == null) return;
+
+//         flatten(root.right);
+//         flatten(root.left);
+
+//         root.right = prev;
+//         root.left = null;
+//         prev = root;
+//     }
+// }
 class Solution {
-    private TreeNode prev = null;
-
     public void flatten(TreeNode root) {
-        if (root == null) return;
-
-        flatten(root.right);
-        flatten(root.left);
-
-        root.right = prev;
-        root.left = null;
-        prev = root;
+        TreeNode cur = root;
+        
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode temp = cur.right;
+                
+                cur.right = cur.left;
+                cur.left = null;
+                
+                TreeNode tl = cur.right;
+                while (tl.right != null) {
+                    tl = tl.right;
+                }
+                
+                tl.right = temp;
+            }
+            
+            cur = cur.right;
+        }
     }
 }
