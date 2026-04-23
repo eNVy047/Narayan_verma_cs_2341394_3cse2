ae6c8f41f6dfa89c90a866602cea1c670b1570ed
Narayan Verma <148738788+eNVy047@users.noreply.github.com>
2026-04-23 10:54:15 +0530

    Time: 2 ms (52.2%), Space: 52.7 MB (80.07%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,19 +1,51 @@
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         ListNode a = headA;
+//         ListNode b = headB;
+//         while (a != b) {
+//             if (a == null) {
+//                 a = headB;
+//             } else {
+//                 a = a.next;
+//             }
+//             if (b == null) {
+//                 b = headA;
+//             } else {
+//                 b = b.next;
+//             }
+//         }
+//         return a;
+//     }
+// }
+
 public class Solution {
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode a = headA;
-        ListNode b = headB;
-        while (a != b) {
-            if (a == null) {
-                a = headB;
-            } else {
-                a = a.next;
-            }
-            if (b == null) {
-                b = headA;
-            } else {
-                b = b.next;
+
+        ListNode fir = headA;
+        while (fir.next != null) {
+            fir = fir.next;
+        }
+
+        fir.next = headB;
+        ListNode slow = headA;
+        ListNode fast = headA;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                slow = headA;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                fir.next = null;
+                return slow;
             }
         }
-        return a;
+        fir.next = null;
+        return null;
     }
 }
